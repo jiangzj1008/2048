e295d45c66db6e67ee6cf06906daca452d8c7e04
jiangzj1008 <jiangzj1008@outlook.com>
2017-06-07 13:46:19 +0800

    初始文件

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-// 打开时随机位置，生成一个2或4
+var e = function(sel){
+    var el = document.querySelector(sel)
+    return el
+}
 
-// 
+var initScreen = function() {
+    var html = e('html')
+    var rate = document.documentElement.clientHeight/document.documentElement.clientWidth
+    if (rate < 1.5) {
+        html.style.fontSize = document.documentElement.clientHeight/603*312.5+"%"
+    } else {
+        html.style.fontSize = document.documentElement.clientWidth/375*312.5+"%"
+    }
+}
+
+initScreen()
+
+// 打开时随机位置，生成一个2或4
+var randomNum = function() {
+    
+}
+
+// 滑动时
+// 1、所有滑快向同一方向滑动
+// 2、遇到数字相同的滑块，合并升级
+// 3、滑动结束，在剩余的空间里，随机生成一个2/4
